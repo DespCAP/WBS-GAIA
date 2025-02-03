dad1659ee459e59367f55ca7e3f8ec5bdd994ddd
Amoy Ashesh <120006090+DespCAP@users.noreply.github.com>
2025-02-04 00:56:41 +0530

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -17,5 +17,23 @@ class training:
     drop_columns = ['solution_id', 'designation', 'source_id', 'target']
     target_column = 'target'
 
-# class ml:
-    
+class ml:
+    def __init__(self):
+        self.ml_algs_dict = {
+            'RFC':[], 
+            'LR':[], 
+            'SVM (RBF)':[], 
+            'DTC':[], 
+            'AdaBoost':[], 
+            'KNN': [], 
+            'NB':[], 
+            'Bagging': []}
+
+    def add_data(self, category, data_list):
+        if category in self.ml_algs_dict:
+            self.ml_algs_dict[category].extend(data_list)
+        else:
+            print(f"Category '{category}' not found in the dictionary.")
+
+    def get_data(self, category):
+        return self.ml_algs_dict.get(category, [])
